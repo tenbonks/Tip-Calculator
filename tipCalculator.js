8c24b79349bdcf4e00c1456e3f1f44062affef41
Ben Tonks <thebentonks@gmail.com>
2021-02-24 21:04:43 +0000

    + logic to the results labels depending on headcount

--- tipCalculator.js
+++ tipCalculator.js
@@ -1,37 +1,44 @@
-document.querySelector('#tip-form').onchange = function(){
+document.querySelector('#tip-form').onchange = function () {
 
     // Get the values from the form
-   var billTotal = Number(document.getElementById("billtotal").value);
-   var personCount = Number(document.getElementById("headcount").value);
-   var tip = document.getElementById("tipInput").value; 
-   
-   // The code below will get the value of the range input
-    // and display the percentage value to the user
-   document.getElementById("tipOutput").innerHTML = `${tip}%`;
+    var billTotal = Number(document.getElementById("billtotal").value);
+    var personCount = Number(document.getElementById("headcount").value);
+    var tip = document.getElementById("tipInput").value;
 
-    if (personCount <= 0) {
+    //set some label ID's to clean code below
+    var tipAmountLabel = document.getElementById("tipAmountLabel");
+    var totalBillWithTipLabel = document.getElementById("totalBillWithTipLabel");
+
+    // The code below will get the value of the range input
+    // and display the percentage value to the user
+    document.getElementById("tipOutput").innerHTML = `${tip}%`;
+
+    // If the person count is less than 1 set it to 1, change the labels if person count is more than 1
+    if (personCount <= 1) {
         personCount = 1
+        tipAmountLabel.innerHTML = "Tip"
+        totalBillWithTipLabel.innerHTML = "Bill and Tip"
+        document.getElementById("headcount").value = personCount
+    } else {
+        tipAmountLabel.innerHTML = `Tip ÷ ${personCount}`
+        totalBillWithTipLabel.innerHTML = `Bill and Tip ÷ ${personCount}`
     }
 
     // Equate the values given by user    
-   var tipValue = billTotal * (tip/100)
+    var tipValue = billTotal * (tip / 100)
 
-   var finalBill = billTotal + tipValue
-   
+    var finalBill = billTotal + tipValue
 
-   console.log(finalBill)
 
     // Select the HTML elements within the results section
-   var tipAmount = document.querySelector('#tipAmount');
-   var totalBillWithTip = document.querySelector('#totalBillWithTip');
+    var tipAmount = document.querySelector('#tipAmount');
+    var totalBillWithTip = document.querySelector('#totalBillWithTip');
 
     // Set the selected elements values to the ones equated above this
     //  Divide the tip value by person count   
-   tipAmount.value = (tipValue / personCount).toFixed(2);
-   totalBillWithTip.value = finalBill.toFixed(2);
-
-   console.log("£" + tipAmount)
+    tipAmount.value = (tipValue / personCount).toFixed(2);
+    totalBillWithTip.value = (finalBill / personCount).toFixed(2);
 
     // Show the results once this code has ran
-    document.getElementById("results").style.display="block"
+    document.getElementById("results").style.display = "block"
 }
